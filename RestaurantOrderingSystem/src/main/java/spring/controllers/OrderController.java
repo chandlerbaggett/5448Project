da5885171c315e86073d6a33594cbb61aea426db
Kristopher Neilsen <kamaro67@gmail.com>
2017-04-12 21:23:43 -0600

    Added updates to OrderController

--- RestaurantOrderingSystem/src/main/java/spring/controllers/OrderController.java
+++ RestaurantOrderingSystem/src/main/java/spring/controllers/OrderController.java
@@ -1,46 +1,66 @@
 package spring.controllers;
 
+import spring.models.MenuItem;
 import spring.models.Order;
+import spring.models.OrderItem;
+import spring.models.OrderMemento;
 
 public class OrderController {
 	
-	Order order;
+	Order order;	
+	User customer;
+	RealRestaurant restaurant;
 	
 	public Order createOrder(){
-		this.order = new Order();
+		this.order = new Order();		
+		orderHistory = customer.getOrderHistory();
+		orderHistory.addOrder(order);		
 		return order;
 	}
 	
 	public void submitOrder(Order order){
-		//ADD CODE
+		if (restaurant.isOpen()){
+			order.setOrderStatus("Submitted");
+			OrderHistory orderHistory = restaurant.getOrders();
+			orderHistory.addOrder(order);
+		}
+		else{
+			System.out.println("Restaurant is currently closed. Order not submitted.");
+		}
 	}
 	
 	public void cancelOrder(Order order){
-		//ADD CODE
+		order.setOrderStatus("Cancelled");
 	}
 	
-	public void saveOrder(Order order){
-		//ADD CODE
+	public void saveOrder(Order order){		
+		order.createMemento();
 	}
 	
-	public Order resumeOrder(Order order){
-		//ADD CODE
+	public Order resumeOrder(){
+		OrderMemento memento = new OrderMemento();
+		order.setMemento(memento);
+		return order;
 	}
 	
-	public void addToOrder(OrderItem orderitem, Order order){
-		//ADD CODE
+	public void addToOrder(MenuItem menuItem, Order order){
+		OrderItem orderItem = new OrderItem(menuItem);
+		order.addItem(orderItem);
 	}
 	
-	public void removeFromOrder(OrderItem orderitem, Order order){
-		//ADD CODE
+	public void removeFromOrder(OrderItem orderItem, Order order){
+		order.removeItem(orderItem);
 	}
 	
 	public double displayTotal(Order order){
-		//ADD CODE
+		return order.calculateOrderTotal();
 	}
 	
 	public Order duplicateOrder(){
-		//ADD CODE
+		Order duplicate = order.clone();
+		//order.setorderId();
+		order.setOrderStatus("Active");
+		return duplicate;
 	}
 
 }
