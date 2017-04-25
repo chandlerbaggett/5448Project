ca1409c8331d47af0a3229205bc313de2834f8a0
Samuel Steward <scs.steward@gmail.com>
2017-04-24 19:34:04 -0600

    annotated staff list and sub components for use in hibernate

--- RestaurantOrderingSystem/src/main/java/spring/models/Permission.java
+++ RestaurantOrderingSystem/src/main/java/spring/models/Permission.java
@@ -1,11 +1,18 @@
 package spring.models;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Permission {
 	private boolean canMakeOrders;
 	private boolean canViewRestaurantOrders;
 	private boolean canEditRestaurantStaff;
 	private boolean canEditRestaurantMenu;
 	
+	public Permission() {
+		super();
+	}
+
 	public Permission(boolean canMakeOrders, boolean canViewRestaurantOrders, boolean canEditRestaurantStaff,
 			boolean canEditRestaurantMenu) {
 		super();
