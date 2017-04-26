270167369f45684be3ca02115434530482d57b3d
Samuel Steward <scs.steward@gmail.com>
2017-04-25 22:55:44 -0600

    added true login with checking against the DB for users

--- RestaurantOrderingSystem/src/main/java/spring/config/AuthenticationService.java
+++ RestaurantOrderingSystem/src/main/java/spring/config/AuthenticationService.java
@@ -4,30 +4,39 @@ import java.util.Arrays;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import spring.models.OrderHistory;
+import spring.models.User;
+import utils.DBManager;
+
 @Service
 public class AuthenticationService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//TODO add actual database access for getting user
-		if (username.equals("username")) {
-			spring.models.User userInfo = new spring.models.User();
-			userInfo.setUserName("username");
-			userInfo.setPassword("password");
+		if (DBManager.getModel(User.class, 1) == null) {
+			User user = new User();
+			user.setUserName("username");
+			user.setPassword("password");
 			
+			user.setDisplayName("username");
+			user.setOrderHistory(new OrderHistory());
+			
+			DBManager.saveModel(user);
+		};
+		
+		User user = DBManager.getUser(username);
+		
+		if (user != null) {	
 			GrantedAuthority authority = new SimpleGrantedAuthority("USER");
-			UserDetails userDetails = (UserDetails) new User(userInfo.getUserName(), userInfo.getPassword(), Arrays.asList(authority));
+			UserDetails userDetails = (UserDetails) new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), Arrays.asList(authority));
 			return userDetails;
 			
 		} else {
 			UsernameNotFoundException excpt = new UsernameNotFoundException(username);
 			throw excpt;
 		}
-		
-		
 	}
 } 
