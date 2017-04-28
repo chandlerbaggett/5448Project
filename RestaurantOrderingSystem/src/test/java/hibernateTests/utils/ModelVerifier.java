ce63dd403fbce3e710d7a6e73218609da0e9c5da
Samuel Steward <scs.steward@gmail.com>
2017-04-27 22:57:28 -0600

    added error messages to asserts

--- RestaurantOrderingSystem/src/test/java/hibernateTests/utils/ModelVerifier.java
+++ RestaurantOrderingSystem/src/test/java/hibernateTests/utils/ModelVerifier.java
@@ -12,10 +12,10 @@ import spring.models.User;
 
 public class ModelVerifier {
 	public static void verifyUsers(User user1, User user2) {
-		   assertTrue(user1.getId() == user2.getId());
-		   assertEquals(user1.getUserName(), user2.getUserName());
-		   assertEquals(user1.getPassword(), user2.getPassword());
-		   assertEquals(user1.getDisplayName(), user2.getDisplayName());
+		   assertTrue("ids should be equal", user1.getId() == user2.getId());
+		   assertEquals("user names should match", user1.getUserName(), user2.getUserName());
+		   assertEquals("passwords should match", user1.getPassword(), user2.getPassword());
+		   assertEquals("display names should match", user1.getDisplayName(), user2.getDisplayName());
 		   
 		   verifyOrderHistories(user1.getOrderHistory(), user2.getOrderHistory());
 	}
@@ -34,21 +34,21 @@ public class ModelVerifier {
 	}
 
 	public static void verifyStaffMembers(StaffMember member, StaffMember member2) {
-		assertTrue(member.getId() == member2.getId());
+		assertTrue("ids should be equal", member.getId() == member2.getId());
 		verifyUsers(member.getUser(), member2.getUser());
 		verifyPermissions(member.getPermission(), member.getPermission());
 	}
 
 	public static void verifyPermissions(Permission permission, Permission permission2) {
-		assertTrue(permission.canEditRestaurantMenu() == permission2.canEditRestaurantMenu());
-		assertTrue(permission.canEditRestaurantStaff() == permission2.canEditRestaurantStaff());
-		assertTrue(permission.canMakeOrders() == permission2.canMakeOrders());
-		assertTrue(permission.canViewRestaurantOrders() == permission2.canViewRestaurantOrders());		
+		assertTrue("permissions should match", permission.canEditRestaurantMenu() == permission2.canEditRestaurantMenu());
+		assertTrue("permissions should match", permission.canEditRestaurantStaff() == permission2.canEditRestaurantStaff());
+		assertTrue("permissions should match", permission.canMakeOrders() == permission2.canMakeOrders());
+		assertTrue("permissions should match", permission.canViewRestaurantOrders() == permission2.canViewRestaurantOrders());		
 	}
 	
 	public static void verifyStaffLists(StaffList list1, StaffList list2) {
-		assertTrue(list1.getId() == list2.getId());
-		assertTrue(list1.getStaffMembers().size() == list2.getStaffMembers().size());  
+		assertTrue("ids should be equal", list1.getId() == list2.getId());
+		assertTrue("sizes should be equal", list1.getStaffMembers().size() == list2.getStaffMembers().size());  
 		
 		for (int x=0; x < list1.getStaffMembers().size(); x++) {
 			verifyStaffMembers(list1.getStaffMembers().toArray(new StaffMember[0])[x],
