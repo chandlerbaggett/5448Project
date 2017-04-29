06910b6c61eaef765379b42549addbbc9e6e2f5c
Samuel Steward <scs.steward@gmail.com>
2017-04-28 23:31:36 -0600

    added real data access to the main menu

--- RestaurantOrderingSystem/src/main/java/spring/controllers/MainMenuController.java
+++ RestaurantOrderingSystem/src/main/java/spring/controllers/MainMenuController.java
@@ -4,59 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import spring.models.Menu;
-import spring.models.OrderHistory;
 import spring.models.Permission;
-import spring.models.RealRestaurant;
-import spring.models.Restaurant;
-import spring.models.StaffList;
-import spring.models.User;
 import utils.DBManager;
+import utils.TestDataBuilder;
 
 @Controller
 public class MainMenuController {
 	@GetMapping("/home")
-	public String welcome(Model model) {
-		model.addAttribute("firstName", "Elizabeth");
+	public String home(Model model) {
+		if (DBManager.getRestaurant() == null) {
+			TestDataBuilder.buildTestData();
+		}
 		
-		buildRestaurant();
+		Permission permission = DBManager.getRestaurant().getStaff().getPermissionForStaff(DBManager.getLoggedInUser());
 		
-		Permission permission = new Permission(false, true, false, false);
-		
-		model.addAttribute("showPlacedOrders", permission.canViewRestaurantOrders());
-		model.addAttribute("showPlaceOrder", permission.canMakeOrders());
-		model.addAttribute("showEditMenu", permission.canEditRestaurantMenu());
-		model.addAttribute("showManageStaff", permission.canEditRestaurantStaff());
+		if (permission != null) {
+			System.out.println("have permission");
+			model.addAttribute("showPlacedOrders", permission.canViewRestaurantOrders());
+			model.addAttribute("showPlaceOrder", permission.canMakeOrders());
+			model.addAttribute("showEditMenu", permission.canEditRestaurantMenu());
+			model.addAttribute("showManageStaff", permission.canEditRestaurantStaff());
+			
+		} else {
+			System.out.println("no permission");
+			model.addAttribute("showPlacedOrders", false);
+			model.addAttribute("showPlaceOrder", true);
+			model.addAttribute("showEditMenu", false);
+			model.addAttribute("showManageStaff", false);
+		}
 		
 		return "mainMenu";
-	}
-	
-	//TODO extract into fake model builder class
-	private void buildRestaurant() {
-		RealRestaurant restaurant = new RealRestaurant();
-		
-		User staffUser = new User();
-		staffUser.setUserName("steve");
-		staffUser.setPassword("pass");
-		
-		staffUser.setDisplayName("steve");
-		staffUser.setOrderHistory(new OrderHistory());
-		
-		DBManager.saveModel(staffUser);
-		
-		Permission permission = new Permission(false, true, false, false);
-		
-		StaffList staff = new StaffList();
-		staff.addStaffMember(staffUser, permission);
-	
-		
-		restaurant.setStaff(staff);
-		restaurant.setMenu(new Menu());
-		restaurant.setName("name");
-		restaurant.setIsOpen(true);
-		restaurant.setLocation("palce");
-		restaurant.setOrders(new OrderHistory());
-		
-		DBManager.saveModel(restaurant);
 	}
 }
